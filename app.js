05a115b417e538447edb0d49dc211e15917647a3
sandjonyves <sandjonyves@gmail.com>
2025-09-26 23:37:02 +0200

    fix. depoyement 17

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cors({
   origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
 
