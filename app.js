6f22851110903a8eb294bad0c67731a2208525fb
sandjonyves <sandjonyves@gmail.com>
2025-09-27 06:37:43 +0200

    test

--- app.js
+++ app.js
@@ -26,7 +26,18 @@ const app = express();
 
 // ================= CORS ===================
 app.use(cors({
-  origin: '*',
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:8081',
+    'http://localhost:19006',
+    'exp://localhost:19000',
+    'http://192.168.162.150:3001',
+    'http://192.168.162.150:3000',
+    'http://192.168.162.150:8081',
+    'http://192.168.162.150:19006',
+    'exp://192.168.160.150:8081',
+    'exp://192.168.160.150:8082'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
